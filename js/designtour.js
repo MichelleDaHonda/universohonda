86e0cfc0a7b2dd5eb14d06a5e53879cfd202d0ff
MichelleDaHonda <112778813+MichelleDaHonda@users.noreply.github.com>
2022-09-06 21:17:45 -0300

    Add files via upload

---
+++ js/designtour.js
@@ -0,0 +1,42 @@
+const btnConceito = document.getElementById('btn-conceito');
+const btnCiclistica = document.getElementById('btn-ciclistica');
+
+
+function mostrarP(){
+    const p = document.getElementById('p-conceito');
+    const design = document.getElementById('design-bg');
+    const pCiclistica = document.getElementById('p-ciclistica');
+    const btnConceito = document.getElementById('btn-conceito');
+   
+
+    
+    p.style.display = 'block';
+    p.style.position = 'absolute';
+    design.style.backgroundImage = "url(./img/tourfoto4.webp)";
+    pCiclistica.style.display = 'none';
+    btnConceito.style.borderBottom = '3px solid #c00c00';
+    btnCiclistica.style.borderBottom = 'none';
+   
+    
+}
+
+function mostrarPCiclistica(){
+    const pCiclistica = document.getElementById('p-ciclistica');
+    const btnCiclistica = document.getElementById('btn-ciclistica');
+    const design = document.getElementById('design-bg');
+    const p = document.getElementById('p-conceito');
+   
+    pCiclistica.style.display = 'block';
+    pCiclistica.style.position = 'absolute';
+    design.style.backgroundImage = "url(./img/tourfoto4.webp)";
+    p.style.display = 'none';
+
+    btnCiclistica.style.borderBottom = '3px solid #c00c00';
+    btnConceito.style.borderBottom = 'none';
+   
+}
+
+
+
+btnConceito.addEventListener('click', mostrarP);
+btnCiclistica.addEventListener('click', mostrarPCiclistica);
